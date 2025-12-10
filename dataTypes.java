7d0464c86de4849c33109d575560d235920ea138
Bharath14-raj <nampallybharath65@gmail.com>
2025-12-10 15:36:34 +0530

    dataTypes.java

---
+++ dataTypes.java
@@ -0,0 +1,16 @@
+class dataTypes{
+    public static void main(String args[]){
+        int myNum = 8;
+        char myletter = 'B';
+        float myfloat = 4.56f;
+        boolean myBool = true;
+        String myText = "Hello";
+        System.out.println(myNum);
+        System.out.println(myfloat);
+        System.out.println(myBool);
+        System.out.println(myletter);
+        System.out.println(myText);
+
+
+    }
+}
